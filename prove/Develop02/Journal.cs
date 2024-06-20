928257ac2c15f065b48e2b50df3b4981ca4a8592
MSeries-kelvin <aramiyorogheneserome@gmail.com>
2024-06-20 10:58:50 +0100

    Teams Meeting - Week02

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
