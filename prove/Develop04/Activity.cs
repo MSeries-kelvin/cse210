00ccebcbd0f6119a663d9c4d406359b28c702d25
MSeries-kelvin <aramiyorogheneserome@gmail.com>
2024-06-29 12:46:56 +0100

    Week04 - stub created

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,33 @@
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    // Constructors
+    public Activity()
+    {
+
+    }
+
+    // Methods
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        
+    }
+}
---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,27 @@
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>();
+
+    // Constructors
+    public ListingActivity()
+    {
+        
+    }
+
+    // Methods
+    public void Run()
+    {
+        
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return new List<string>();
+    }
+}
