dea115cbd43431ee2fc62af137bb3db3a4c9e1a1
MSeries-kelvin <aramiyorogheneserome@gmail.com>
2024-06-29 12:48:30 +0100

    Week04 - stub created

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        // the student instance automatically has the GetName() method!
+        Student student = new Student();
+        string name = student.GetName();
+        Console.WriteLine(name);
+    }
+}
+
+// a regular class called Person
+public class Person
+{
+    public string GetName()
+    {
+        return "Joseph";
+    }
+}
+
+// a class that inherits from Person
+public class Student : Person
+{
+    public string GetNumber()
+    {
+        return "0123456789";
     }
 }
